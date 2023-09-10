201a403458bf42b64f6d89144bb5289a4d33b18e
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2023-09-10 19:33:01 +0200

    baner + about section

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,19 @@
 const hamburger = document.querySelector(".js-hamburger");
 const navList = document.querySelector(".js-nav-list");
 const active = "active";
+let scrollDisabled = false;
+
+function toggleScroll() {
+  if (scrollDisabled) {
+    document.body.style.overflow = "visible";
+  } else {
+    document.body.style.overflow = "hidden";
+  }
+  scrollDisabled = !scrollDisabled;
+}
 
 hamburger.addEventListener("click", () => {
   navList.classList.toggle(active);
   hamburger.classList.toggle(active);
+  toggleScroll();
 });
