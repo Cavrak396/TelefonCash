8be9eca2a1db451bc77fee636e1894f17a711f70
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2023-09-20 23:55:28 +0200

    lastest upgrade

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,80 @@
 "use strict";
 
-const hamburger = document.querySelector(".js-hamburger");
-const navList = document.querySelector(".js-nav-list");
 const active = "active";
-let scrollDisabled = false;
+const black = "black";
+const originalBackgroundColor = "";
 
-function toggleScroll() {
-  if (scrollDisabled) {
-    document.body.style.overflow = "visible";
-  } else {
-    document.body.style.overflow = "hidden";
-  }
-  scrollDisabled = !scrollDisabled;
+function toggleScroll(disable) {
+  document.body.style.overflow = disable ? "hidden" : "visible";
 }
 
-hamburger.addEventListener("click", () => {
-  navList.classList.toggle(active);
-  hamburger.classList.toggle(active);
-  toggleScroll();
+function scrollToSection(targetClass) {
+  const targetSection = document.querySelector(`.${targetClass}`);
+  if (targetSection) {
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+function posaljiMejl() {
+  const emailInput = document.getElementById("emailInput");
+  const email = emailInput.value;
+
+  if (email) {
+    const url = `mailto:cavrakmarko396@gmail.com?subject=Novi mejl&body=${email}`;
+    window.location.href = url;
+  } else {
+    alert("Unesite Vaše pitanje!");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".js-hamburger");
+  const navList = document.querySelector(".js-nav-list");
+  const navLinks = document.querySelectorAll(".js-nav-link");
+
+  function handleHamburgerClick() {
+    const header = document.querySelector(".js-header");
+    header.style.transition = "background-color 0.8s";
+    header.style.backgroundColor =
+      header.style.backgroundColor === black ? originalBackgroundColor : black;
+
+    navList.classList.toggle(active);
+    hamburger.classList.toggle(active);
+
+    if (navList.classList.contains(active)) {
+      toggleScroll(true);
+    } else {
+      toggleScroll(false);
+    }
+
+    setTimeout(() => {
+      header.style.transition = "";
+    }, 600);
+  }
+
+  hamburger.addEventListener("click", handleHamburgerClick);
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const targetClass = link.getAttribute("href").substring(1);
+      scrollToSection(targetClass);
+      navList.classList.remove(active);
+      hamburger.classList.remove(active);
+      toggleScroll(false);
+
+      setTimeout(() => {
+        const header = document.querySelector(".js-header");
+        header.style.transition = "background-color 0.8s";
+        header.style.backgroundColor = originalBackgroundColor;
+        setTimeout(() => {
+          header.style.transition = "";
+        }, 800);
+      }, 10);
+    });
+  });
+
+  document
+    .getElementById("sendEmailButton")
+    .addEventListener("click", posaljiMejl);
 });
